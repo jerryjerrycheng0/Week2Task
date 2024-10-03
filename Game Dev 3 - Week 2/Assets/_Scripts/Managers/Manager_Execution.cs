c2ec48af1a7e802d3fac9240354fcc3a6ea8e887
jerryjerrycheng0 <jerryjerrycheng0@gmail.com>
2024-10-03 21:28:20 +0100

    Refactored the Scripts to use Namespaces

--- Game Dev 3 - Week 2/Assets/_Scripts/Managers/Manager_Execution.cs
+++ Game Dev 3 - Week 2/Assets/_Scripts/Managers/Manager_Execution.cs
@@ -1,51 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameDevWithMarco.ScriptableObjects;
 
-public class Manager_Execution : MonoBehaviour
+namespace GameDevWithMarco.Managers
 {
-    /// <summary>
-    /// This script will be used to execute the methods from the other little scripts we 
-    /// have created.
-    /// The reason to I do this, is to create script with single responsibilities.
-    /// </summary>
-
-
-
-    //This variable will contain an array of gun scriptable objects
-    public GunsType_ScriptableObject[] gunScriptableObjects;
-
-    //This variable will be used to see what scriptable object is currently in use
-    [SerializeField] private GunsType_ScriptableObject activeScriptableObject;
-
-    //Reference to the gun ui manager
-    Manager_WeaponUi gunUiScript;
-    //Reference to the gun 3d manager
-    Manager_Gun3dManager gun3dScript;
-
-
-
-    void Start()
+    public class Manager_Execution : MonoBehaviour
     {
-        //Fills the variables with references to the other scripts
-        FindScripts();
-        //Executes the methods from the other scripts, and uses the data from the first S.O. in the array
-        FirstRun();
-    }
+        /// <summary>
+        /// This script will be used to execute the methods from the other little scripts we 
+        /// have created.
+        /// The reason to I do this, is to create script with single responsibilities.
+        /// </summary>
 
-    private void FindScripts()
-    {
-        gunUiScript = FindObjectOfType<Manager_WeaponUi>();
-        gun3dScript = FindObjectOfType<Manager_Gun3dManager>();
-    }
 
-    private void FirstRun()
-    {
-        //Will set the variable to be the first s.o. in the array
-        activeScriptableObject = gunScriptableObjects[0];
 
-        //Will execute the methods from the other scripts
-        gunUiScript.AssignUI(activeScriptableObject);
-        gun3dScript.SpawnGun(activeScriptableObject);
+        //This variable will contain an array of gun scriptable objects
+        public GunsType_ScriptableObject[] gunScriptableObjects;
+
+        //This variable will be used to see what scriptable object is currently in use
+        [SerializeField] private GunsType_ScriptableObject activeScriptableObject;
+
+        //Reference to the gun ui manager
+        Manager_WeaponUi gunUiScript;
+        //Reference to the gun 3d manager
+        Manager_Gun3dManager gun3dScript;
+
+
+
+        void Start()
+        {
+            //Fills the variables with references to the other scripts
+            FindScripts();
+            //Executes the methods from the other scripts, and uses the data from the first S.O. in the array
+            FirstRun();
+        }
+
+        private void FindScripts()
+        {
+            gunUiScript = FindObjectOfType<Manager_WeaponUi>();
+            gun3dScript = FindObjectOfType<Manager_Gun3dManager>();
+        }
+
+        private void FirstRun()
+        {
+            //Will set the variable to be the first s.o. in the array
+            activeScriptableObject = gunScriptableObjects[0];
+
+            //Will execute the methods from the other scripts
+            gunUiScript.AssignUI(activeScriptableObject);
+            gun3dScript.SpawnGun(activeScriptableObject);
+        }
     }
 }
--- Game Dev 3 - Week 2/Assets/_Scripts/Managers/Manager_WeaponUi.cs
+++ Game Dev 3 - Week 2/Assets/_Scripts/Managers/Manager_WeaponUi.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using GameDevWithMarco.ScriptableObjects;
 
-public class Manager_WeaponUi : MonoBehaviour
-{   
-
-    //References to the UI elements that we want to change 
-    public TMP_Text gunName;
-    public TMP_Text gunSubTitle;
-    public TMP_Text gunDescription;
-    public Image gunSymbol;
-
-
-
-    //The method has a parameter of type scp_GunsType_ScriptableObject in 
-    //order to be versatile when we need to use it.
-    public void AssignUI(GunsType_ScriptableObject currentSO)
+namespace GameDevWithMarco
+{
+    public class Manager_WeaponUi : MonoBehaviour
     {
-        //These lines of code will assign the data we have in the s.o. to the Ui
-        gunName.text = currentSO.gunName;
-        gunSubTitle.text = currentSO.gunSubTitle;
-        gunDescription.text = currentSO.gunDescription;
-        gunSymbol.sprite = currentSO.gunSymbol.sprite;
-    }
 
+        //References to the UI elements that we want to change 
+        public TMP_Text gunName;
+        public TMP_Text gunSubTitle;
+        public TMP_Text gunDescription;
+        public Image gunSymbol;
+
+
+
+        //The method has a parameter of type scp_GunsType_ScriptableObject in 
+        //order to be versatile when we need to use it.
+        public void AssignUI(GunsType_ScriptableObject currentSO)
+        {
+            //These lines of code will assign the data we have in the s.o. to the Ui
+            gunName.text = currentSO.gunName;
+            gunSubTitle.text = currentSO.gunSubTitle;
+            gunDescription.text = currentSO.gunDescription;
+            gunSymbol.sprite = currentSO.gunSymbol.sprite;
+        }
+
+    }
 }
